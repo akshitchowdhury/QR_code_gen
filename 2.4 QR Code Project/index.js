9c42172553f52c40eca9ca3fd81424c989c1d213
Pratikshit Chowdhury <101483800+akshitchowdhury@users.noreply.github.com>
2023-07-17 11:00:10 +0530

    Add files via upload

--- 2.4 QR Code Project/index.js
+++ 2.4 QR Code Project/index.js
@@ -3,3 +3,28 @@
 2. Use the qr-image npm package to turn the user entered URL into a QR code image.
 3. Create a txt file to save the user input using the native fs node module.
 */
+
+
+import qr from 'qr-image'
+import inquirer from 'inquirer';
+
+
+inquirer
+  .prompt([
+    /* Pass your questions in here */
+    {message: "Type in the url",
+        name: 'URL',}
+    
+])
+  .then((answers) => {
+    // Use user feedback for... whatever!!
+   const url = answers.URL;
+    console.log(answers)
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
